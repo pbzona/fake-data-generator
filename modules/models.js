aba2dda61cc28e6961a064e04a03ee74e9bdc3a2
Phil Zona <phil.b.zona@gmail.com>
2018-08-02 15:18:12 -0500

    Default to space when object not passed into constructors

--- modules/models.js
+++ modules/models.js
@@ -1,32 +1,32 @@
 class Medication {
   constructor(medication) {
-    this.name = medication.name;
-    this.dose = medication.dose;
-    this.frequency = medication.frequency;
-    this.starting = medication.starting;
-    this.ending = medication.ending;
-    this.physician = medication.physician;
-    this.purpose = medication.purpose;
+    this.name = medication.name || ' ';
+    this.dose = medication.dose || ' ';
+    this.frequency = medication.frequency || ' ';
+    this.starting = medication.starting || ' ';
+    this.ending = medication.ending || ' ';
+    this.physician = medication.physician || ' ';
+    this.purpose = medication.purpose || ' ';
   }
 }
 
 class Surgery {
   constructor(surgery) {
-    this.date = surgery.date;
-    this.procedure = surgery.procedure;
-    this.physician = surgery.physician;
-    this.hospital = surgery.hospital;
-    this.notes = surgery.notes;
+    this.date = surgery.date || ' ';
+    this.procedure = surgery.procedure || ' ';
+    this.physician = surgery.physician || ' ';
+    this.hospital = surgery.hospital || ' ';
+    this.notes = surgery.notes || ' ';
   }
 }
 
 class Illness {
   constructor(illness) {
-    this.illness = illness.illness;
-    this.start = illness.start;
-    this.end = illness.end;
-    this.hospital = illness.hospital;
-    this.notes = illness.notes;
+    this.illness = illness.illness || ' ';
+    this.start = illness.start || ' ';
+    this.end = illness.end || ' ';
+    this.hospital = illness.hospital || ' ';
+    this.notes = illness.notes || ' ';
   }
 }
 
